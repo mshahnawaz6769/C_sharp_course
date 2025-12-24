b65a3c0d00d9c3bac1749443bef84a07ba4ff42b
mshahnawaz6769 <mshahnawaz6769@gmail.com>
2025-12-24 11:09:03 +0500

    Add C# project files

---
+++ Program.cs
@@ -0,0 +1,334 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+// What is C#?
+// C# is pronounced "C-Sharp".
+// It is an object-oriented programming language created by Microsoft that runs on the .NET Framework.
+// C# has roots from the C family, and the language is close to other popular languages like C++ and Java.
+// The first version was released in year 2002. The latest version, C# 13, was released in November 2024.
+// C# is used for:
+// Mobile applications, Desktop applications, Web applications, Web services, Web sites,Games, VR, Database applications
+// And much, much more!
+// Why Use C#?
+// It is one of the most popular programming languages in the world
+// It is easy to learn and simple to use
+// It has huge community support
+// C# is an object-oriented language which gives a clear structure to programs and allows code to be reused, lowering development costs
+// As C# is close to C, C++ and Java, it makes it easy for programmers to switch to C# or vice versa
+
+
+
+// For Creating Project
+// cmd -> dotnet new console -o project_name
+
+// console function syntax
+
+// Console.WriteLine("Hello, World!");  // For Printing Data
+// Console.Write("My self shahnawaz"); // This Console.write is used to multiple data in one line
+// Console.WriteLine(); //is used in C# to display output on the console (screen) and then move the cursor to the next line.
+
+// varaibles and DataTypes;
+// Type: int, string, bool, char, double, long, array, object, class, interface
+
+// int number = 6769; // int: store whole number
+// Console.WriteLine(number);
+
+// long largeNumber = 456451234245656456; // long: store large whole number
+// Console.WriteLine(largeNumber);
+
+// string name = "shahnawaz"; // string: store text
+// Console.WriteLine(name);
+
+// double decimalNumber = 88.09; // double: store decimal number
+// Console.WriteLine(decimalNumber);
+
+// bool isStudent = true; // bool: check statement
+// Console.WriteLine(isStudent);
+
+// char A = 'A'; // char: store only one character
+// Console.WriteLine(A);
+
+
+
+// array method
+
+// int[] num= {1,2,3,4,5};
+// Console.WriteLine(num[2]); // One data print for an array
+
+// int[] num= {1,2,3,4,5};
+// for(int i = 0; i<=num.Length; i++) { // For multiple Data Create loop
+//     Console.WriteLine(num[i]);
+//     // Console.Write(num[i] + "\t");
+// }
+
+// int[] num = {1,2,3,4,5};
+// for(int i=0; i < num.Length; i++) {
+//     for(int j=0; j<=i; j++) {
+//         Console.WriteLine(num[i]+ "\t");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// int[] num = {1, 2, 3, 4, 5};
+
+// for (int i = 0; i < num.Length; i++)
+// {
+//     for (int j = 0; j <= i; j++)
+//     {
+//         Console.Write(num[i] + "\t"); // prints on same line
+//     }
+//     Console.WriteLine(); // moves to new line after each row
+// }
+
+// int a = 4, b = 5;
+// int a = Math.Min(5, 10);
+// Console.WriteLine(a);
+
+// int a = Math.Max(5, 10);
+// Console.WriteLine(a);
+
+
+// Calculator
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// string op = Console.ReadLine();
+
+// if (op == "+")
+// {
+//     Console.WriteLine(num1 + num2);
+// }
+// else if (op == "-")
+// {
+//     Console.WriteLine(num1 - num2);
+// }
+// else if (op == "*")
+// {
+//     Console.WriteLine(num1 * num2);
+// }
+// else if (op == "/")
+// {
+//     if (num2 != 0)
+//         Console.WriteLine(num1 / num2);
+//     else
+//         Console.WriteLine("Cannot divide by zero");
+// }
+// else
+// {
+//     Console.WriteLine("Invalid operator");
+// }
+
+// function, if_else, switch case, loops, break, continue, -> similar as javaScript
+
+// Object Oriented Programs
+// 1. Inheritance
+// Single Inheritance, Multilevel Inheritance, Multiple Inheritance, Hierarchical Inheritance, Hybrid Inheritance.
+// 2. Encapsulation
+// public, private, protected.
+// 3. Polimorpism
+// method overloading, method overriding
+// 4. Abstraction
+
+
+// class and method
+// class Animal
+// {
+//     public string name = "cat";
+//     public void naming()
+//     {
+//         Console.WriteLine("hello");
+//     }
+// }
+
+// class Programs
+// {
+//     static void Main(string[] args)
+//     {
+//         Animal an1 = new Animal();
+//       an1.naming();
+//     }
+// }
+
+
+// class method constructor
+
+// class Student
+// {
+//     public string name;
+//     public int age;
+//     public Student(string st_name, int st_age)
+//     {
+//         name = st_name;
+//         age = st_age;
+//     }
+// }
+
+// class Programs
+// {
+//     static void Main(string[] args)
+//     {
+//         Student st1 = new Student("mariyaaaa", 420);
+//         Console.WriteLine(st1.name);
+//     }
+// }
+
+
+// Practice Questions (OOP Basics)
+
+// Q1. Class & Object
+// Create a class Student with:
+// variables: name, rollNo
+// method: displayDetails()
+// Create an object and print the student details.
+
+// class Student {
+//     public string name = "shahnawaz";
+//     public int rollNo = 19;
+
+//     public void displayDetails() {
+//         Console.WriteLine("Name: " + name);
+//         Console.WriteLine("Roll No: " + rollNo);
+//     }
+
+// }
+
+// class Programs{
+//     static void Main(string[] args) {
+//         Student st1 = new Student();
+//         st1.name = "shahnawaz";
+//         st1.rollNo = 19;
+//         st1.displayDetails();
+//     }
+// }
+
+// Q2. Default Constructor
+// Create a class Car that:
+// has variables brand, speed
+// uses a default constructor to initialize values
+// displays the values using a method
+
+// class Car{
+//     public string brand;
+//     public int speed;
+
+//     public Car(string cr_brand, int cr_speed) {
+//         brand = cr_brand;
+//         speed = cr_speed;
+//     }
+// }
+
+// class Program {
+//     static void Main(string[] args) {
+//         Car cr_info = new Car("BMW M5", 560);
+//         Console.WriteLine(cr_info.brand + " " + cr_info.speed);
+//     }
+// }
+
+// Q3. Parameterized Constructor
+// Create a class Book with:
+// variables: title, author, price
+// a parameterized constructor
+// a method to display book details
+
+// class Book {
+//     public string title;
+//     public string author;
+//     public int price;
+    
+//     public Book(string b_title, string b_author, int b_price) {
+//         title = b_title;
+//         author = b_author;
+//         price = b_price;
+//     }
+
+//     public void display() {
+//         Console.Write(title + author + price);
+//     }
+// }
+
+// class Program{
+//     static void Main(string[] args) {
+//         Book sellBook = new Book("Maths", "Algebra", 210);
+//         sellBook.display();
+//     }
+// }
+
+// Q4. Constructor Overloading
+// Create a class Rectangle that:
+// has variables length, width
+// one constructor with no parameters
+// one constructor with two parameters
+// method to calculate and display area
+
+// class Rectangle{
+//     public int length;
+//     public int width;
+
+//     public Rectangle(){
+
+//     }
+
+//     public Rectangle(int r_length, int r_width) {
+//         length = r_length;
+//         width = r_width;
+//     }
+
+//     public int Area() {
+//         return length * width;
+//     }
+// }
+
+// class Program{
+//     static void Main(string[] args) {
+//         Rectangle shape = new Rectangle(3,4);
+//         Console.WriteLine(shape.Area());
+//     }
+// }
+
+// Q5. Object Creation Using Constructor
+// Create a class Employee with:
+// variables: id, name, salary
+// constructor to initialize all variables
+// create two objects with different values and display them
+
+// class Employee{
+//     public int id;
+//     public string name;
+//     public int salary;
+
+//     public Employee(int e_id, string e_name, int e_salary) {
+//         id = e_id;
+//         name = e_name;
+//         salary = e_salary;
+//     }
+//     public void Display() {
+//         Console.WriteLine( id + " " + name + " " + salary);
+    
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Employee emp1 = new Employee(101, "Alice", 50000);
+//         Employee emp2 = new Employee(102, "Bob", 60000);
+
+//         emp1.Display();
+//         emp2.Display();
+//     }
+// }
+
+
+// Object Oriented Programs
+// 1. Inheritance
+// Single Inheritance, Multilevel Inheritance, Multiple Inheritance, Hierarchical Inheritance, Hybrid Inheritance.
+// 2. Encapsulation
+// public, private, protected.
+// 3. Polimorpism
+// method overloading, method overriding
+// 4. Abstraction
+
+// Inheritance Topics
+
